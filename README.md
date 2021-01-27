81923730834c8c1e456008b94302c5d89573811e
hernandoagf <22449631+hernandoagf@users.noreply.github.com>
2021-01-27 11:39:44 -0300

    Added route for handling file upload

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # [File Metadata Microservice](https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/file-metadata-microservice)
+
+# File Metadata Microservice
+
+> File metadata microservice using Express. Part of FreeCodeCamp curriculum
+> projects.
+
+You can find the
+[FCC Project here](https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/file-metadata-microservice)
+
+And the [Live app here]()
+
+## Usage
+
+Install dependencies
+
+```bash
+npm install
+```
+
+### Run server
+
+```bash
+npm run dev   # Express :5000
+```
--- server.js
+++ server.js
@@ -1,20 +1,36 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express')
+const cors = require('cors')
 require('dotenv').config()
 
-var app = express();
+const multer = require('multer')
+const upload = multer()
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+const app = express()
+
+app.use(cors())
+app.use('/public', express.static(process.cwd() + '/public'))
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+
+  if (!req.file) return res.status(400).json({ msg: 'Please choose a file to upload' })
+
+  const { originalname, mimetype, size } = req.file
+
+  res.status(200).json({ 
+    name: originalname,
+    type: mimetype,
+    size
+   })
+})
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-});
+    res.sendFile(process.cwd() + '/views/index.html')
+})
 
 
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
-});
+})
